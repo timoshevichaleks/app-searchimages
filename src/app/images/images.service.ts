1f2cd31687930f1bb11e86395a837788306eeeb9
timoshevichaleks <alti2692@gmail.com>
2021-09-23 14:39:27 +0300

    production

--- src/app/images/images.service.ts
+++ src/app/images/images.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from "@angular/common/http";
-import { environment } from "../../environments/environment";
+import { environment } from "../../environments/environment.prod";
 import { map } from "rxjs/operators";
 import { PhotoResponse } from "./models/photo-response";
 import { BehaviorSubject, Observable } from "rxjs";
